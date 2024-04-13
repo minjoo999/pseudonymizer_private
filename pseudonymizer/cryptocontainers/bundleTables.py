08b14d920eb34ada9d11187ad960dc7a06188060
Minjoo Kim <sjctk1@gmail.com>
2024-04-14 08:53:43 +0900

    Add New Scripts by MJ

--- pseudonymizer/cryptocontainers/bundleTables.py
+++ pseudonymizer/cryptocontainers/bundleTables.py
@@ -2,28 +2,23 @@ from abc import ABC, abstractmethod
 
 
 class BundleTables(ABC):
-    """결합키 테이블 저장, 결합대상정보 테이블 저장 클래스를 위한 추상화 클래스"""
+    """테이블을 저장하는 추상 클래스"""
     @abstractmethod
-    def addInitTables(self):
-        """원본테이블, 결합키 생성 테이블, 결합대상정보 테이블로 이루어진 InitTables 클래스 받는 추상 메서드"""
-        pass
-
-    @abstractmethod
-    def selectTables(self):
-        """InitTables에서 해당 클래스의 성격에 맞는 테이블만 골라내어 저장하기"""
+    def selectTables(self, data):
+        """DBContainer에서 선언한 스키마.테이블 중 목적에 맞는 테이블을 저장하는 추상 메서드"""
         pass
 
     @abstractmethod
     def getTableList(self):
-        """클래스 성격에 맞는 테이블을 출력"""
+        """selectTables에서 선택한 데이터를 반환하는 추상 메서드"""
         pass
 
     @abstractmethod
     def getSchemas(self):
-        """스키마명만 모아서 출력"""
+        """selectTables에서 선택한 스키마명 반환하는 추상 메서드"""
         pass
-
+    
     @abstractmethod
     def getTables(self):
-        """테이블명만 모아서 출력"""
-        pass
+        """selectTables에서 선택한 테이블명 반환하는 추상 메서드"""
+        pass    
