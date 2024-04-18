f4956d545b510a0585358f5c1ab6d5ee34fb62cb
ksydata <ksydata01@gmail.com>
2024-04-18 20:07:56 +0900

    Test source code 3.7 Error by SY

--- pseudonymizer/pseudonymizers/addressMaskingModule.py
+++ pseudonymizer/pseudonymizers/addressMaskingModule.py
@@ -1,5 +1,5 @@
 from pseudonymizer.pseudonymizer import Pseudonymizer
-
+import re
 
 class AddressMaskingModule(Pseudonymizer):
     """
@@ -12,18 +12,20 @@ class AddressMaskingModule(Pseudonymizer):
         
     def pseudonymizeData(self, address: str):
         """주소의 일부에 대한 마스킹을 수행하는 메서드"""    
-        # 시군구 단위
-        if self.masking_loca == "시군구":
-            # 시군구는 앞의 3글자 유지하고 나머지는 마스킹
-            return address[:3] + '*' * (len(address)-3)
-        # 시군구 읍면동/행정동 단위
-        elif self.masking_loca == "읍면동":
-            # 주소를 공백을 기준으로 나누어 리스트로 변환
-            parts = address.split()
-            if len(parts) > 1:
+        # 주소 정규식 패턴
+        pattern = r"(\S+시|\S+도|\S+특별시|\S+특별자치시|\S+특별자치도) (\S+구|\S+시|\S+군) (\S+(읍|면|동))"
+        
+        # 입력받은 주소가 정규식 패턴과 일치하는지 확인
+        pattern_match = re.match(pattern, address)
+
+        if pattern_match:
+            # 시군구 단위로 주소를 마스킹하는 경우
+            if self.masking_loca == "시군구":
+                # 시군구는 앞의 3글자 유지하고 나머지는 마스킹
+                return pattern_match.group(1)[:3] + '*' * (len(pattern_match.group(1))-3) + ' ' + pattern_match.group(2) + ' ' + pattern_match.group(3)
+            # 시군구 읍면동/행정동 단위로 주소를 마스킹하는 경우
+            elif self.masking_loca == "읍면동":
                 # 마지막 요소는 마스킹하지 않고 그대로 반환
-                return ' '.join(parts[:-1]) + ' ' + parts[-1]
-            else:
-                return address
+                return pattern_match.group(1) + ' ' + pattern_match.group(2) + ' ' + pattern_match.group(3)
         else:
-            print("입력받은 {}은(는) 주소가 아닙니다.".format(address))
+            print("입력받은 {}은(는) 유효한 주소 형식이 아닙니다.".format(address))
