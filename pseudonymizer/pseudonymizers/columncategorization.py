3df36114f43a5e3eb3fd63a186cfcde49caf7f2b
Minjoo Kim <sjctk1@gmail.com>
2024-04-18 23:29:15 +0900

    Test source code 3.11 by MJ

--- pseudonymizer/pseudonymizers/columncategorization.py
+++ pseudonymizer/pseudonymizers/columncategorization.py
@@ -9,16 +9,16 @@ class CategorizationOfColumn(Pseudonymizer):
         self.right = right
         self.ascending = ascending
     
-    def pseudonymizeData(self, df):
+    def pseudonymizeData(self, dataseries):
         """식별성이 높은 그룹을 하나로 묶는 메서드"""
         if self.numeric_type == "bin":
-            return self.pseudonymizeAmountbyBin(df, self.grouping_standard, self.right, self.ascending)
+            return self.pseudonymizeAmountbyBin(dataseries, self.grouping_standard, self.right, self.ascending)
         elif self.numeric_type == "pct":
-            return self.pseudonymizeAmountbyPct(df, self.grouping_standard, self.right, self.ascending)        
+            return self.pseudonymizeAmountbyPct(dataseries, self.grouping_standard, self.right, self.ascending)        
         else: 
             raise ValueError(f"{self.numeric_type}은 유효한 범주화 기법 적용 유형이 아닙니다.")
 
-    def makeLabels(self, num_type, df, grouping_standard, ascending: bool):
+    def makeLabels(self, num_type, dataseries, grouping_standard, ascending: bool):
         """범주화 중 필요한 label을 만들어주는 클래스"""
         if num_type == "pct":
             labels = []
@@ -45,13 +45,13 @@ class CategorizationOfColumn(Pseudonymizer):
         elif num_type == "bin":
             labels = []
             if isinstance(grouping_standard, int):
-                bins = pd.cut(df, bins=grouping_standard).unique()
+                bins = pd.cut(dataseries, bins=grouping_standard).unique()
                 for category in bins:
                     start_value = category.left
                     end_value = category.right
                     labels.append(f"{start_value} ~ {end_value}")
             else:
-                category_values = pd.cut(df, bins=grouping_standard).value_counts(sort=False)
+                category_values = pd.cut(dataseries, bins=grouping_standard).value_counts(sort=False)
                 for category in list(category_values.index):
                     start_value = category.left
                     end_value = category.right
@@ -64,25 +64,25 @@ class CategorizationOfColumn(Pseudonymizer):
         else:
             raise ValueError(f"{num_type}은 유효한 범주화 기법 적용 유형이 아닙니다.")
 
-    def pseudonymizeAmountbyBin(self, df, grouping_standard, right: bool, ascending: bool):
+    def pseudonymizeAmountbyBin(self, dataseries, grouping_standard, right: bool, ascending: bool):
         """기타 금액 구간별 범주화 메서드
         신용공여금액(예: 한도/건별대출, 담보대출, 리스/카드할부금융서비스 등)의 일정 급간화
         pd.cut 활용하여 범주화하되, 입력값 및 오름차순/내림차순, 급간 선택에 따라 파라미터를 달리함
         """
         if isinstance(grouping_standard, list):
             num = len(grouping_standard)
-            df = pd.cut(df, bins = num, labels = grouping_standard, right = right)
-            return df
+            data = pd.cut(dataseries, bins = num, labels = grouping_standard, right = right)
+            return data
 
         elif isinstance(grouping_standard, int):
-            labels = self.makeLabels("bin", df, grouping_standard, ascending = ascending)                              
-            df = pd.cut(df, bins = grouping_standard, labels = labels, right = right)
-            return df
+            labels = self.makeLabels("bin", dataseries, grouping_standard, ascending = ascending)                              
+            data = pd.cut(dataseries, bins = grouping_standard, labels = labels, right = right)
+            return data
 
         else:
             raise ValueError("grouping_standard는 list 또는 int 타입으로 입력해 주십시오.")
             
-    def pseudonymizeAmountbyPct(self, df, grouping_standard, right: bool, ascending: bool):
+    def pseudonymizeAmountbyPct(self, dataseries, grouping_standard, right: bool, ascending: bool):
         """기타 금액 백분위에 의한 범주화 메서드
         개인사업자의 추청매출액/평당월임대료를 백분위수에 따라 매출등급화(90~100%, 65~90%, 35~65%, 10~35%, 0~10%)
 
@@ -90,15 +90,15 @@ class CategorizationOfColumn(Pseudonymizer):
         - 백분위수 = ((등수 - 1) / 컬럼 레코드 갯수) * 100
         - 백분위수별로 pd.cut
         """
-        rank_df = df.copy()
-        rank_df["rank"] = rank_df.rank(method='min')
+        rank_dataseries = dataseries.copy()
+        rank_dataseries["rank"] = rank_dataseries.rank(method='min')
 
         percentiles = []
-        for rank in rank_df["rank"]:
-            percentile = ((rank - 1) / len(rank_df['rank'])) * 100
+        for rank in rank_dataseries["rank"]:
+            percentile = ((rank - 1) / len(rank_dataseries['rank'])) * 100
             percentiles.append(percentile)
-        rank_df["percentile"] = percentiles
-        new_labels = self.makeLabels("pct", rank_df, grouping_standard, ascending)
+        rank_dataseries["percentile"] = percentiles
+        new_labels = self.makeLabels("pct", rank_dataseries, grouping_standard, ascending)
 
         if isinstance(grouping_standard, int):
             # 디버깅 파트(int 타입에 안맞게 len(grouping_standard)로 발생하는 오류)
@@ -108,5 +108,5 @@ class CategorizationOfColumn(Pseudonymizer):
             unique_sorted_bins = sorted(set(grouping_standard))
             bins = [0] + unique_sorted_bins + [100]
 
-        result_df = pd.cut(rank_df["percentile"], bins=bins, labels=new_labels, right=right, duplicates="drop")
-        return result_df
+        result_dataseries = pd.cut(rank_dataseries["percentile"], bins=bins, labels=new_labels, right=right, duplicates="drop")
+        return result_dataseries
