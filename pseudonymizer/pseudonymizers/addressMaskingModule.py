032a4168ac514b91908e3ba3b1761fb55e139996
Minjoo Kim <sjctk1@gmail.com>
2024-04-20 08:33:47 +0900

    Test source code 3.16 by MJ

--- pseudonymizer/pseudonymizers/addressMaskingModule.py
+++ pseudonymizer/pseudonymizers/addressMaskingModule.py
@@ -9,22 +9,64 @@ class AddressMaskingModule(Pseudonymizer):
 
     def pseudonymizeData(self, address):
         """pseudonymizer 클래스 (추상) 기반 구체 메서드"""
-        pattern = r"(\S+[시도])(\s+\S+[시군구])(\s+\S+[읍면동])"
-        pattern_match = re.match(pattern, address)
-
-        # (시군구) (읍면동) 단위까지는 있어야 실행하도록 조건 설정
-        if pattern_match == None:
-            print("주소를 시군구 / 읍면동 단위까지 입력해 주세요")
-        else:
+        if address:
             if self.masking_type == "government_area":
-                print("시군구")
+                self.governmentMasking(address)
             elif self.masking_type == "district":
-                print("읍면동")
+                self.districtMasking(address)
             else:
-                print("올바른 masking_type을 입력해주세요.")
+                print("masking_type을 정확한 형태로 적어주세요.")
+        else:
+            pass
 
-        # 특별시 / 광역시 / 특별자치시 (서울특별시, 인천광역시, 세종특별자치시 등)
+    @classmethod
+    def governmentMasking(cls, address):
+        """시군구 레벨 마스킹 메서드 (예: 서울시 동작구 / 경기도 고양시)"""
+        total_pattern = r"(\S+[시도])(\s+\S+[시군구])"
 
-        # 도 (경기도, 경상남도 등) / 특별자치도 (제주특별자치도 등)
+        if re.match(total_pattern, address):
+            city_county_pattern = r"(\S+[시도])"
+            government_pattern = r"(\b\S+[시군구])"
+            result = []
 
-        # 예외처리
+            if re.match(city_county_pattern, address):
+                result.append(re.match(city_county_pattern, address).group(0))
+                
+            if re.search(government_pattern, address):
+                result.append(re.search(government_pattern, address).group(0))
+            
+            # print(result)
+
+            return ' '.join(result)
+        else:
+            print("데이터가 시도 / 시군구 입력 형식에 맞지 않습니다.")
+        
+
+    @classmethod
+    def districtMasking(cls, address):
+        """읍면동 레벨 마스킹 메서드 (예: 경기도 화성시 반송동 / 서울특별시 동작구 신대방동)"""
+        total_pattern = r"(\S+[시도])(\s+\S+[시군구])(\s+\S+[읍면동])"
+
+        if re.match(total_pattern, address):
+            city_county_pattern = r"(\S+[시도])"
+            government_pattern = r"(\b\S+[시군구])"
+            district_pattern = r"(\s+\S+[읍면동])"
+
+            result = []
+
+            if re.match(city_county_pattern, address):
+                result.append(re.match(city_county_pattern, address).group(0))
+            if re.search(government_pattern, address):
+                result.append(re.search(government_pattern, address).group(0))
+            if re.search(district_pattern, address):
+                result.append(re.search(district_pattern, address).group(0))
+        
+            return ' '.join(result)
+        else:
+            print("데이터가 시도 / 시군구 / 읍면동 입력 형식에 맞지 않습니다.")
+
+        
+    # 예외처리 (classmethod)
+    @classmethod
+    def addressdef(cls):
+        pass
